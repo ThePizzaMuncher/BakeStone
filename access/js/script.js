011cc0ae25af27919128ae777901379627053996
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2022-12-15 08:14:39 +0100

    v0.5

--- access/js/script.js
+++ access/js/script.js
@@ -1,4 +1,39 @@
+function draw() { 
 
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.fillRect(30, 30, 50, 150);
+    const canvas = document.getElementById('canvas'); 
+    
+     
+    
+     if (canvas.getContext) { 
+    
+     const ctx = canvas.getContext('2d'); 
+    
+     var steenDx = 210; var steenDy = 50; var steenDz = 100;  
+    
+     var voegDx = 10; var voegDy = 10;        
+    
+        var muurDx = 3000;    var muurDy = 2000; 
+    
+        var koppenMaat = steenDz+ voegDx; 
+    
+        var lagenMaat = steenDy + voegDy; 
+    
+         
+    
+        for (let ypos=0 ;ypos < muurDy;ypos += lagenMaat){ 
+    
+          for (let xpos=0 ;xpos < muurDx;xpos += (2 * koppenMaat)){ 
+    
+              ctx.strokeRect(10 + xpos , 10 + ypos , steenDx , steenDy ); 
+    
+          } 
+    
+        } 
+    
+      } 
+    
+    } 
+    
+     
+    
+    draw(); 
