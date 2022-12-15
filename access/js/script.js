636731fe77dc874235415e569914ebb75dd9537f
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2022-12-15 13:36:44 +0100

    Button opmaak en functies

--- access/js/script.js
+++ access/js/script.js
@@ -1,14 +1,34 @@
-function teken() {
-    const canvas = document.getElementById("canvas");
+const canvas = document.getElementById("canvas");
+if (canvas.getContext) {
+    var ctx = canvas.getContext("2d");
+    var steenDx = 210;
+    var steenDy = 50;
+    var steenDz = 100;
+    var voegDx = 10;
+    var voegDy = 10;
+    var muurDx = 3000;
+    var muurDy = 2000;
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+    var koppenMaat = steenDz + voegDx;
+    var lagenMaat = steenDy + voegDy;
+    for (let ypos = 0; ypos < muurDy; ypos += lagenMaat) {
+        for (let xpos = 0; xpos < muurDx; xpos += (2 * koppenMaat)) {
+            ctx.strokeRect(10 + xpos, 10 + ypos, steenDx, steenDy);
+        }
+    }
+}
+function BGDD_website() {
+    window.open("https://BGDD.nl", '_blank');
+}
+document.getElementById("#stone1").addEventListener("click", () => {//Knop steen 1 listener.
+    canvas.width = canvas.width;
     if (canvas.getContext) {
         var ctx = canvas.getContext("2d");
-        var steenDx = 210;
-        var steenDy = 50;
-        var steenDz = 100;
-        var voegDx = 10;
-        var voegDy = 10;
-        var muurDx = 3000;
-        var muurDy = 2000;
+        var steenDx = 215;
+        var steenDy = 101;
+        ctx.strokeStyle = 'red';
+        ctx.stroke();
         var koppenMaat = steenDz + voegDx;
         var lagenMaat = steenDy + voegDy;
         for (let ypos = 0; ypos < muurDy; ypos += lagenMaat) {
@@ -17,10 +37,4 @@ function teken() {
             }
         }
     }
-}
-function BGDD_website() {
-    window.open("https://bgdd.nl", '_blank');
-}
-function deur() {
-    ctx.fillRect(200, 200, 100, 100);
-}
+});
