819239a8c22f4fe8c81d3ab99a08aa4568cb966d
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2022-12-16 14:31:25 +0100

    Update script.js

--- access/js/script.js
+++ access/js/script.js
@@ -1,40 +1,35 @@
-const canvas = document.getElementById("canvas");
-if (canvas.getContext) {
-    var ctx = canvas.getContext("2d");
-    var steenDx = 210;
-    var steenDy = 50;
-    var steenDz = 100;
-    var voegDx = 10;
-    var voegDy = 10;
-    var muurDx = 700;
-    var muurDy = 2000;
-    ctx.strokeStyle = 'red';
-    ctx.stroke();
-    var koppenMaat = steenDz + voegDx;
-    var lagenMaat = steenDy + voegDy;
-    for (let ypos = 0; ypos < muurDy; ypos += lagenMaat) {
-        for (let xpos = 0; xpos < muurDx; xpos += (2 * koppenMaat)) {
-            ctx.strokeRect(10 + xpos, 10 + ypos, steenDx, steenDy);
-        }
-    }
-}
-function BGDD_website() {
-    window.open("https://BGDD.nl/", '_blank');
-}
-document.getElementById("#stone1").addEventListener("click", () => {//Knop steen 1 listener.
-    canvas.width = canvas.width;
+var steenDx = 210;
+var steenDy = 50;
+var steenDz = 100;
+var voegDx = 10;
+var voegDy = 10;
+var muurDx = 700;
+var muurDy = 2000;
+function teken() {//Algemene teken functie
+    const canvas = document.getElementById("canvas");
     if (canvas.getContext) {
         var ctx = canvas.getContext("2d");
-        var steenDx = 215;
-        var steenDy = 101;
         ctx.strokeStyle = 'red';
         ctx.stroke();
         var koppenMaat = steenDz + voegDx;
         var lagenMaat = steenDy + voegDy;
         for (let ypos = 0; ypos < muurDy; ypos += lagenMaat) {
             for (let xpos = 0; xpos < muurDx; xpos += (2 * koppenMaat)) {
-                ctx.strokeRect(10 + xpos, 10 + ypos, steenDx, steenDy);
+            ctx.strokeRect(10 + xpos, 10 + ypos, steenDx, steenDy);
             }
         }
     }
+}
+teken();
+function BGDD_website() {//Doorverwijzing naar BGDD website.
+    window.open("https://BGDD.nl/", '_blank');
+}
+document.getElementById("#stone1").addEventListener("click", () => {//Knop steen 1 listener.
+    cv_cls();
+    steenDx = 250;
+    steenDy = 50;
+    teken();
 });
+function cv_cls() {//Cls voor canvas.
+    canvas.width = canvas.width;
+}
