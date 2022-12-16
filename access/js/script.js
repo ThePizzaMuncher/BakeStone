d7cd4c37af91ede6640702b454b60422c3e7cf3e
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2022-12-16 17:59:29 +0100

    Update script.js v0.1

--- access/js/script.js
+++ access/js/script.js
@@ -6,12 +6,6 @@ var voegDy = 10;
 var muurDx = 500;
 var muurDy = 300;
 function teken() {//Algemene teken functie.
-    //pre install functions.
-    if (muurDx < 1 || muurDy < 1) {
-        muurDx = 10000;
-        muurDy = 10000;
-    }
-    //tekenaar.
     const canvas = document.getElementById("canvas");
     if (canvas.getContext) {
         var ctx = canvas.getContext("2d");
@@ -25,40 +19,55 @@ function teken() {//Algemene teken functie.
         }
     }
 }
-teken();
+teken();//Hier wordt getekend.
 function BGDD_website() {//Doorverwijzing naar BGDD website.
     window.open("https://BGDD.nl/", '_blank');
 }
-document.getElementById("#stone1").addEventListener("click", () => {
+document.getElementById("#stone1").addEventListener("click", () => {//Knop steen 1 listener. (Waalformaat baksteen)
     cv_cls();
     get_B_en_H();
+    muur_B_en_H_check();
     steenDx = 210;
     steenDy = 50;
     voegDx = 10;
     teken();
 });
-document.getElementById("#stone2").addEventListener("click", () => {//Knop steen 2 listener.
+document.getElementById("#stone2").addEventListener("click", () => {//Knop steen 2 listener. (Dikformaat baksteen)
     cv_cls();
     get_B_en_H();
+    muur_B_en_H_check();
     steenDx = 215;
     steenDy = 101;
     voegDx = 12;
     teken();
 });
-document.getElementById("#stone3").addEventListener("click", () => {
+document.getElementById("#stone3").addEventListener("click", () => {//Knop steen 3 listener. (Ysselformaat baksteen)
     cv_cls();
     get_B_en_H();
-    steenDx = 210;
-    steenDy = 100;
-    voegDx = 10;
+    muur_B_en_H_check();
+    steenDx = 160;
+    steenDy = 78;
+    voegDx = -15.5;
     teken();
 });
 function cv_cls() {//Cls voor canvas.
     canvas.width = canvas.width;
 }
-function get_B_en_H() {
-    let $breedte = document.getElementById("$breedte").value;
-    let $hoogte = document.getElementById("$hoogte").value;
-    muurDx = $breedte;
-    muurDy = $hoogte;
+function get_B_en_H() {//Stel hoogte en breedte in, in canvas.
+    muurDx = document.getElementById("$breedte").value;
+    muurDy = document.getElementById("$hoogte").value;
+}
+function muur_B_en_H_check() {//Kijk als hoogte en breedte groter is dan 0.
+    if (muurDy < 1) {//Als y-as kleiner is dan 0, doe dan...
+        window.alert("Voer een groter getal in voor de hoogte.");
+        muurDx = 500;
+        muurDy = 300;
+    }
+    else {
+        if (muurDx < 1) {//Als x-as kleiner is dan 0, doe dan...
+            window.alert("Voer een groter getal in voor de breedte.");
+            muurDx = 500;
+            muurDy = 300;
+        }
+    }
 }
